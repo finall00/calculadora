57a121ab49724ad4e9a92381ea5b7e1bb2f3e3ac
sussu_69 <rodrigorodrigues7200218@gmail.com>
2024-05-22 10:22:23 -0300

    mod: IntervaloProporcao

--- src/componentes/pages/IntervaloProporcao.tsx
+++ src/componentes/pages/IntervaloProporcao.tsx
@@ -2,13 +2,28 @@ import Botao from "../Button";
 import Input from "../Input";
 
 export default function IntervaloConfiacaProporcao (){
+
+  function handleSubmit(e:React.FormEvent<HTMLFormElement>){
+    e.preventDefault()
+
+    const formData = new FormData(e.currentTarget)
+    const data = Object.fromEntries(formData) as {sucesso:string, grau:string, amostra:string}
+    
+    const {sucesso, grau, amostra} =data
+
+    if(!sucesso || !grau || !amostra){
+        alert("Prencha todos os campos")
+        return
+    }
+  }
+
     return(
       <div>
-        <form action="">
+        <form onSubmit={handleSubmit}>
             <Input type="text" id="sucesso" name="sucesso" placeholder="Digite o sucesso"/>
             <Input type="text" id="grau" name="grau" placeholder="Grau de confiança"/>
             <Input type="text" id="amostra" name="amostra" placeholder="Tamanho da amostra"/>
-            <Botao>C</Botao>
+            <Botao type="submit">Calcular</Botao>
         </form>
       </div>
     )
