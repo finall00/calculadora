f78f0aca51b4dbec68703e1576e0cf2b6f70daff
finall00 <vvcruzpelissoni08@gmail.com>
2024-05-26 13:07:30 -0300

    add: calcProporcao

---
+++ src/lib/CalcProp.ts
@@ -0,0 +1,9 @@
+export function calcularProporcao(quantidade: number, total: number): number{
+ 
+        if(total ===0){
+            throw new Error("O total não pode ser zero.");
+            
+        }
+    
+    return quantidade / total;
+}
