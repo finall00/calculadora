f3b5e4738d1ca26693ec86865304835d1667d965
oliRenan <oliveirarenan479@gmail.com>
2024-06-01 09:48:53 -0300

    falto esse

--- src/componentes/SelectGrauConfianca.tsx
+++ src/componentes/SelectGrauConfianca.tsx
@@ -2,7 +2,8 @@ import { SelectHTMLAttributes } from "react";
 
 export default function SelectGrauConfianca({...props}: SelectHTMLAttributes<HTMLSelectElement>){
     return(
-        <select {...props}>
+        <select className="block w-96 border border-neutral-300 rounded p-2 mt-3 focus:outline-green-400 hover:border-green-400 text-sm "
+         {...props}>
             <option value="">Selecione o Grau de confiança</option>
             <option value="90%">90%</option>
             <option value="95%">95%</option>
